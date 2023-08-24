ba2053a7696ed23471fd129ebb624ba36c0c26b6
Yinuo Jing <45665026+jynkris1016@users.noreply.github.com>
2023-08-24 14:21:37 +0800

    Update xclip.py

--- models/xclip.py
+++ models/xclip.py
@@ -42,8 +42,8 @@ class XCLIP(CLIP):
             context_length, vocab_size, transformer_width, transformer_heads, transformer_layers
         )
         
-        self.text_prompts_generator = AnimalSpecificPrompt(layers=prompts_layers, embed_dim=embed_dim, alpha=prompts_alpha,)
-        self.video_prompts_generator = AnimalSpecificPrompt(layers=prompts_layers, embed_dim=embed_dim, alpha=prompts_alpha,)
+        self.prompts_generator1 = AnimalSpecificPrompt(layers=prompts_layers, embed_dim=embed_dim, alpha=prompts_alpha,)
+        self.prompts_generator2 = AnimalSpecificPrompt(layers=prompts_layers, embed_dim=embed_dim, alpha=prompts_alpha,)
         self.use_cache=use_cache
         self.mit = MultiframeIntegrationTransformer(T=T, embed_dim=embed_dim, layers=mit_layers,)
 
@@ -166,12 +166,12 @@ class XCLIP(CLIP):
         
         # image_feature: 16,196,512
 
-        text_features = text_features + self.text_prompts_generator(text_features, animal_text_feature) # 16, 140, 512     16, 1, 512
+        text_features = text_features + self.prompts_generator1(text_features, animal_text_feature) # 16, 140, 512     16, 1, 512
         
         # text_feature: 16,140,512
         # video_features: 16,512
         video_features = video_features.unsqueeze(1)
-        video_features = video_features + self.video_prompts_generator2(video_features, animal_text_feature)
+        video_features = video_features + self.prompts_generator2(video_features, animal_text_feature)
         video_features = video_features.squeeze(1)
         
         video_features = video_features / video_features.norm(dim=-1, keepdim=True)
